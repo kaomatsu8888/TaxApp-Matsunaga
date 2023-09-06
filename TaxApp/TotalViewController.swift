c6cec064abacb7bb5aa8c7273c3c822851c00637
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-09-06 11:19:37 +0900

    インデントズレ修正

--- TaxApp/TotalViewController.swift
+++ TaxApp/TotalViewController.swift
@@ -7,10 +7,10 @@
 import UIKit
 
 class TotalViewController: UITableViewController {
-
+    
     // UI要素のアウトレット接続
     @IBOutlet weak var totalLabel: UILabel!
-
+    
     // 合計金額を保存する変数
     var totalAmount: Double = 0.0
     
@@ -25,12 +25,12 @@ class TotalViewController: UITableViewController {
 
 
 /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
